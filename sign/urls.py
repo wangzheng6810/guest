6e26becdd5cb33535f57a62b5c51e48215280e92
xiaomi <1771715149@qq.com>
2019-12-06 15:56:26 +0800

    写了剩下的接口

---
+++ sign/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from sign import views_if
+
+urlpatterns = [
+#     sign system interface:
+#     ex : /api/add_event/
+#     url(r'^add_event/',views_if.add_event, name='add_event'),
+#     ex : /api/add_guest/
+#     url(r'^add_guest/',views_if.add_guest, name='add_guest'),
+#     ex : /api/add_event_list/
+#     url(r'^add_event_list/',views_if.add_event_list, name='add_event_list'),
+#     ex : /api/add_guest_list/
+#     url(r'^add_guest_list/',views_if.add_guest_list, name='add_guest_list'),
+#     ex : /api/user_sign/
+#     url(r'^user_sign/',views_if.user_sign, name='user_sign'),
+ ]
